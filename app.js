8d4f744526b5d7efda0a8959d186968eee7e45c2
Geoffrey Matthews <97923330+YKTV8@users.noreply.github.com>
2022-10-04 20:34:59 -0700

    update

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const loadText = document.querySelector('.loading-text')
 const bg = document.querySelector('.bg')
 
 let load = 0
-let int = setInterval(blurring,  45);
+let int = setInterval(blurring,  25);
 
 function blurring(){
     load++
